d1fd0c2d3ceb911e42c63c480e108411fec4232b
QuangTran <quangtranduy1209@gmail.com>
2025-11-24 16:40:08 +0700

    update schedule interval

--- dags/kalodata_dag.py
+++ dags/kalodata_dag.py
@@ -3,6 +3,7 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 import pendulum
 
+
 local_tz = pendulum.timezone("Asia/Ho_Chi_Minh")
 
 default_args = {
@@ -12,18 +13,23 @@ default_args = {
 }
 
 with DAG(
-    dag_id='kalodata_crawler_weekly',
+    
+    dag_id='kalodata_crawler_daily', 
     default_args=default_args,
-    description='Cào dữ liệu Kalodata định kỳ 7 ngày/lần',
+    description='Cào dữ liệu Kalodata định kỳ mỗi ngày một lần',
+    
     start_date=datetime(2023, 1, 1, tzinfo=local_tz),
-    schedule_interval='0 0 * * 0', # Chạy lúc 00:00 Chủ Nhật hàng tuần
-    catchup=False, # Không chạy bù các ngày quá khứ
-    tags=['kalodata', 'selenium'],
+   
+    # '0 0 * * *' nghĩa là: Phút 0, Giờ 0, Mọi ngày, Mọi tháng, Mọi thứ
+    # Tức là chạy vào 00:00 hàng ngày
+    schedule_interval='0 0 * * *', 
+   
+    catchup=False, # Không chạy bù quá khứ
+    tags=['kalodata', 'selenium', 'daily'],
 ) as dag:
 
-    
     run_scraper = BashOperator(
-        task_id='run_crawler',
+        task_id='run_crawler_daily',
         
         bash_command='cd /opt/airflow/scripts && xvfb-run --auto-servernum --server-args="-screen 0 1280x1024x24" python -u kalodata.py'
     )
